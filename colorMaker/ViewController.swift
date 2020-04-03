02bb3db20c8328e5f6db152ea3c5abb4e05757d7
Kai-Chin Huang <qaz6209031@gmail.com>
2020-04-03 17:04:41 +0800

    Initial Commit

---
+++ colorMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colorMaker
+//
+//  Created by Kai-Chin Huang on 4/3/20.
+//  Copyright © 2020 Kai-Chin Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
