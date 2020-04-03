6bbc8c09eee48c13f637e9fdc2e77e1df9d12776
Kai-Chin Huang <qaz6209031@gmail.com>
2020-04-03 18:02:48 +0800

    App finish

--- colorMaker/ViewController.swift
+++ colorMaker/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+
+    @IBOutlet weak var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeColor(self)
     }
+    
+    @IBAction func changeColor(_ sender: Any) {
+        let r: CGFloat = CGFloat(redSlider.value)
+        let g: CGFloat = CGFloat(greenSlider.value)
+        let b: CGFloat = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r,green: g, blue: b, alpha:1)
+
+    }
+    
 
 
 }
